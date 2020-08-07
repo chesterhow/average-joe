9da6e337738ded80f2539d3e618dd775a17d6204
Chester How <chesterhow@gmail.com>
2020-08-07 22:50:31 +0800

    feat: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Average Joe Coffeehouse Reviews`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Everything you’ve ever wanted to know about coffeehouses in Singapore.`,
+    author: `@chesterhow`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -33,13 +33,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Average Joe Coffeehouse Reviews`,
+        short_name: `Average Joe Reviews`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFEDDF`,
+        theme_color: `#FFEDDF`,
         display: `minimal-ui`,
-        // icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/favicon/favicon.png`,
       },
     },
     `gatsby-plugin-styled-components`,
